af380fd0bbb98f67dd363979575146194a9744d2
okaloopen <leva38363@minspeak.ru>
2026-04-29 09:19:29 -0700

    Create clone_repo.sh

---
+++ clone_repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Скрипт для клонирования репозитория с детектором сундуков.
+
+REPO_URL="https://github.com/okaloopen/minecraft-chest-yolo26.git"
+
+echo "Клонирование репозитория $REPO_URL"
+git clone "$REPO_URL"
